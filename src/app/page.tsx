56517e651c0163205e1e9929b680dc94dc9dcf3f
ritushreedas2002 <ritushreedas20027@gmail.com>
2024-03-25 00:48:46 +0530

    mini

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,22 @@ import Image from "next/image";
 import background from "@/assets/background.png"
 import Navbar from "@/components/Navbar"
 import Carousal from "@/components/GlobalCyrpto/Carousal"
+import CoinTable from "@/components/GlobalCyrpto/CoinTable"
 const Home=()=>{
   return (
-    <div>
+    <div style={{ 
+      backgroundImage: `url(${background.src})`, // Adjusted for Next.js image import
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      minHeight: '100vh', // Ensure the div takes up at least the height of the viewport
+  
+    }}>
       <Image src={background} alt="background" layout="fill" objectFit="cover" quality={100} />
-        <Navbar/>
-        <Carousal/>
+      <div className="relative z-10"> {/* This div ensures content is above the background image */}
+        <Navbar />
+        <Carousal /> 
+        <CoinTable />
+      </div>
 
     </div>
   )
---
+++ src/components/GlobalCyrpto/Chart.js
@@ -0,0 +1,40 @@
+import ReactApexChart from "react-apexcharts";
+import { useState } from "react";
+
+const Chart = ({ sparkline, priceChange }) => {
+    const [chartOptions] = useState({
+        series: [{
+            data: [...sparkline.price],
+        }],
+        chart: {
+            type: "line",
+            height: 80,
+            sparkline: { enabled: true },
+            animations: { enabled: false },
+        },
+        tooltip: { enabled: false },
+        stroke: { width: 1 },
+        colors: [chartColor()],
+    });
+
+    function chartColor() {
+        if (priceChange <= 0) {
+            // Red for negative change
+            return "#ff3131";
+        } else {
+            // Green for positive change
+            return "#25df3e";
+        }
+    }
+
+    return (
+        <ReactApexChart
+            options={chartOptions}
+            series={chartOptions.series}
+            type="line"
+            height={80}
+        />
+    );
+};
+
+export default Chart;
