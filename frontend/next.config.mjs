48b198d94b952c5741268c52ee438d87279b0975
ritushreedas2002 <ritushreedas20027@gmail.com>
2024-05-02 23:30:13 +0530

    mini

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint:{
+    ignoreDuringBuilds:true,
+  },
   images: {
     domains: ['www.bing.com'], // Add other domains as needed
   },
