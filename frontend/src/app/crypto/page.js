886601b667c77a115293f9d28d3613e87534764b
Arpan De <arpandesp7890@gmail.com>
2024-05-09 00:05:35 +0530

    mini

--- frontend/src/app/crypto/page.js
+++ frontend/src/app/crypto/page.js
@@ -2,12 +2,13 @@
 import React from "react";
 import Head from "next/head";
 import CryptoTable from "./Compon";
+import VirtualPortfolio from "./VirtualPortfolio";
 import WatchList from "@/app/crypto/watchlist/page";
 import Link from "next/link";
 
 const CryptoHome = () => {
   return (
-    <div className="bg-gray-900  min-h-screen text-white">
+    <div className="bg-gray-900   min-h-screen text-white">
       <Head>
         <title>Crypto Dashboard</title>
         <meta
@@ -19,7 +20,10 @@ const CryptoHome = () => {
 
       <div className="p-8 h-screen " style={{ height: "calc(100vh - 2rem)" }}>
         <h1 className="text-3xl font-bold">Cryptocurrency Dashboard</h1>
-        <div className="h-1/2  p-4 m-4 bg-gray-600  relative w-[50%] overflow-y-auto no-scrollbar">
+        <div className="m-4 ">
+          <VirtualPortfolio/>
+        </div>
+        <div className="h-1/2 rounded-xl  p-4 m-4 bg-gray-600  relative w-[43%] overflow-y-auto no-scrollbar">
           <div className="sticky flex justify-end top-0 right-0 z-10 -mt-8">
             <Link href="/">
               <button className="bg-blue-500 hover:bg-blue-700 text-white text-sm font-semibold py-1.5 px-3 rounded">
