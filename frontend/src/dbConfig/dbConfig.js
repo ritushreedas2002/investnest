1ae5ceb4ae5b04eee7e09b3fc05cb31fd3318346
Arpan De <arpandesp7890@gmail.com>
2024-05-17 01:49:45 +0530

    mini

--- frontend/src/dbConfig/dbConfig.js
+++ frontend/src/dbConfig/dbConfig.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export async function connect() {
     try {
-        mongoose.connect("mongodb+srv://arpande:zmxncbv%40123@cluster0.nhcw4dm.mongodb.net/Cluster0?retryWrites=true&w=majority");
+        mongoose.connect("mongodb://arpande:zmxncbv%40123@ac-dhs1i5d-shard-00-00.nhcw4dm.mongodb.net:27017,ac-dhs1i5d-shard-00-01.nhcw4dm.mongodb.net:27017,ac-dhs1i5d-shard-00-02.nhcw4dm.mongodb.net:27017/?ssl=true&replicaSet=atlas-tzfa6f-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0");
         const connection = mongoose.connection;
 
         connection.on('connected', () => {
